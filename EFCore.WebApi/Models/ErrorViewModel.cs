34b7dce0f03ea20313d79ec8bd7ce1113f604159
CleuJunior <cleu.junior@gmail.com>
2023-10-03 10:00:51 -0300

    feat: Initial Project

---
+++ EFCore.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EFCore.WebApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## C# .Net Core ASP.NET
